f5d8f991354e813a003b6125754214bf34f182d7
Martin Henriksen <martin.henriksen.92@gmail.com>
2019-10-18 20:48:56 +0200

    Stylefix

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,18 +2,19 @@
 
 namespace Tests\Unit;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+/**
+ * @internal
+ * @coversNothing
+ */
+final class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
-     *
-     * @return void
      */
-    public function testBasicTest()
+    public function test_basic_test()
     {
-        $this->assertTrue(true);
+        static::assertTrue(true);
     }
 }
